1651c59bda4541a5cd0e41edceed1bdcf4106ed9
manolo2603 <manuelfrancis44@gmail.com>
2022-04-19 21:21:48 -0500

    Valorar arreglado el problema de subir imagen

--- src/hooks/useFirebase.tsx
+++ src/hooks/useFirebase.tsx
@@ -1,8 +1,10 @@
-import { Platform, StyleSheet, Text, View } from 'react-native'
-import React, { useState } from 'react'
+import { Alert, Platform, StyleSheet, Text, View } from 'react-native'
+import React, { useContext, useState } from 'react'
 import storage from '@react-native-firebase/storage';
 import firestore from '@react-native-firebase/firestore';
 import { IComentario, IOficeIcon } from '../interfaces/Peticiones';
+import { SesionContext } from '../context/Sesion/SesionContext';
+
 
 interface Props {
     idTrabajador?: string;
@@ -10,10 +12,12 @@ interface Props {
 
 const useFirebase = () => {
 
+
     const [urlImage, setUrlImage] = useState('')
     const [averageRating, setAverageRating] = useState<number>(0)
     const [foto, setFoto] = useState<string[]>([])
     const [Oficio, setOficio] = useState<IOficeIcon[]>([])
+    const { Sesion } = useContext(SesionContext)
     
     const UploadImage = async (pathImage: string) => {
         const nameFile = pathImage.substring(pathImage.lastIndexOf('/') + 1);
@@ -61,6 +65,51 @@ const useFirebase = () => {
             })
     }
 
+    const ValorarTrabajo = async (calificacion: number, idEmploye: string, idUser: string, comentario:string,  pathImage: string) => {
+        let date: Date = new Date();
+        //console.log(photoImage)
+
+        const nameFile = pathImage.substring(pathImage.lastIndexOf('/') + 1);
+        const uploadUri = Platform.OS === 'ios' ? pathImage.replace('file://', '') : pathImage;
+        const reference = storage().ref(nameFile);
+        const task = reference.putFile(uploadUri);
+        task.on('state_changed', snapshot => {
+            const progress = Math.round((snapshot.bytesTransferred / snapshot.totalBytes) * 100);
+            console.log('Upload is ' + progress + '% done');
+        }, error => {
+            console.log(error);
+        }, async () => {
+            let url = await task.snapshot?.ref.getDownloadURL();
+            //console.log(url)
+            firestore()
+            .collection('Trabajadores').doc(idEmploye).collection('Comentarios')
+            .doc(idUser).set({
+                calificacion: calificacion,
+                comentario: comentario,
+                fotosComentario: url,
+                idCliente: idUser
+            }).then(() => {
+                firestore()
+                    .collection('Usuarios').doc(Sesion.Id).collection('Historial')
+                    .doc().set({
+                        idTrabajador: idEmploye,
+                        fecha: date.toLocaleDateString()
+                    }).then(() => {
+                        firestore()
+                            .collection('Usuarios')
+                            .doc(Sesion.Id).collection('EnCurso').doc(idEmploye)
+                            .delete()
+                            .then(() => {
+                                  Alert.alert("Mensaje", 'Comentario Guardado')
+                            });
+
+                    })
+         })                 
+
+        });
+ 
+    }
+
 
     return {
         urlImage,
@@ -69,7 +118,8 @@ const useFirebase = () => {
         foto,
         averageRating,
         Oficio,
-        GetOficios
+        GetOficios,
+        ValorarTrabajo
     }
 }
 
